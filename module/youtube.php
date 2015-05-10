a4968b0df2bc764ce0ffcfe1184492cc0db3ff69
Soulflare3 <robert[at]sf3soft[dot]net>
2015-05-10 19:20:37 -0400

    Updated Youtube module from API v2 to API v3

--- module/youtube.php
+++ module/youtube.php
@@ -6,7 +6,7 @@ Class Modubot_Youtube extends Modubot_Module {
 	public $helpline = 'parses YouTube links and outputs information about them.';
 	private $cache = array();
 	public $noCommand = true;
-
+	
 	public function match(&$that, &$socket, $data, $matches){
 		if($that->expect($data, 'topicchange', ['channel' => $that->channel($data)]))
 			return;
@@ -21,30 +21,40 @@ Class Modubot_Youtube extends Modubot_Module {
 
 		if(isset($this->cache[$id])){
 			$title = $this->cache[$id]['title'];
-			$uploader = $this->cache[$id]['uploader'];
-			$views = $this->cache[$id]['views'];
-			$this->privmsg($socket, $that->channel($data), "({$sender}) \x02{$title}\x02 {$uploader} - {$views} views.");
+			$channelTitle = $this->cache[$id]['channelTitle'];
+			$viewCount = $this->cache[$id]['viewCount'];
+			$this->privmsg($socket, $that->channel($data), "({$sender}) \x02{$title}\x02 {$channelTitle} - {$viewCount} views.");
 
 			return true;
 		}
 
-		$ydata = file_get_contents("http://gdata.youtube.com/feeds/api/videos/{$id}?v=2&alt=jsonc");
-		$ydata = json_decode($ydata);
-		if(isset($ydata->data, $ydata->data->title)){
-			$ydata = $ydata->data;
-			$title = $ydata->title;
-			$uploader = isset($ydata->uploader) ? "[{$ydata->uploader}]" : '';
-			$views = $ydata->viewCount;
-
+		$serverAPIkey = ""; //v3 of the Youtube Data API requires an API key. Get one at http://console.developers.google.com
+		if(empty($serverAPIkey))
+		{
+			$this->privmsg($socket, $that->channel($data), "({$sender}) No API key set, please have the bot's owner edit line 31 of the Youtube module.");
+		}
+		else
+		{
+		$ydata = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet%2Cstatistics&id={$id}&key=".$serverAPIkey);
+		$ydata = json_decode($ydata,true);
+		if(isset($ydata['items'], $ydata['items'][0]['snippet']['title'])){
+			$ydata = $ydata['items'];
+			$title = $ydata[0]['snippet']['title'];
+			$channelTitle = $ydata[0]['snippet']['channelTitle'];
+			$viewCount = $ydata[0]['statistics']['viewCount'];
+			
 			$this->cache[$id] = array(
 				'title' => $title,
-				'uploader' => $uploader,
-				'views' => $views,
+				'channelTitle' => $channelTitle,
+				'viewCount' => $viewCount,
 				'time' => time()
 			);
-
-			$this->privmsg($socket, $that->channel($data), "({$sender}) \x02{$title}\x02 {$uploader} - {$views} views.");
+			
+			$this->privmsg($socket, $that->channel($data), "({$sender}) \x02{$title}\x02 {$channelTitle} - {$viewCount} views.");
 		}
+		}
+		
+		
 	}
 
 	public function process(&$that, &$socket, $data, $input, $command, $args){
